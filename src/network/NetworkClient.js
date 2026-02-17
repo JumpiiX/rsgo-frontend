a496830f66978d9404ed0848ba728b60897f1190
jumpiix <david@unterguggenberger.ch>
2026-02-17 14:13:19 +0100

    add network client

---
+++ src/network/NetworkClient.js
@@ -0,0 +1,166 @@
+export class NetworkClient {
+    constructor() {
+        this.ws = null;
+        this.playerId = null;
+        this.connected = false;
+        this.onPlayerJoinedCallback = null;
+        this.onPlayerLeftCallback = null;
+        this.onPlayerMovedCallback = null;
+        this.onPlayerShotCallback = null;
+        this.onPlayerHitCallback = null;
+        this.onPlayerDiedCallback = null;
+        this.onPlayerRespawnedCallback = null;
+    }
+
+    connect() {
+        this.ws = new WebSocket('ws://localhost:8080');
+        
+        this.ws.onopen = () => {
+            this.connected = true;
+        };
+        
+        this.ws.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+            this.handleMessage(message);
+        };
+        
+        this.ws.onclose = () => {
+            this.connected = false;
+            setTimeout(() => this.connect(), 3000);
+        };
+        
+        this.ws.onerror = (error) => {
+        };
+    }
+
+    handleMessage(message) {
+        switch (message.type) {
+            case 'welcome':
+                this.playerId = message.player_id;
+                break;
+                
+            case 'player_joined':
+                if (this.onPlayerJoinedCallback && message.player.id !== this.playerId) {
+                    this.onPlayerJoinedCallback(message.player);
+                }
+                break;
+                
+            case 'player_left':
+                if (this.onPlayerLeftCallback) {
+                    this.onPlayerLeftCallback(message.player_id);
+                }
+                break;
+                
+            case 'player_moved':
+                if (this.onPlayerMovedCallback && message.player_id !== this.playerId) {
+                    this.onPlayerMovedCallback(message);
+                }
+                break;
+                
+            case 'player_shot':
+                if (this.onPlayerShotCallback) {
+                    this.onPlayerShotCallback(message);
+                }
+                break;
+                
+            case 'player_hit':
+                if (this.onPlayerHitCallback) {
+                    this.onPlayerHitCallback(message);
+                }
+                break;
+                
+            case 'player_died':
+                if (this.onPlayerDiedCallback) {
+                    this.onPlayerDiedCallback(message);
+                }
+                break;
+                
+            case 'player_respawned':
+                if (this.onPlayerRespawnedCallback) {
+                    this.onPlayerRespawnedCallback(message);
+                }
+                break;
+        }
+    }
+
+    joinGame(playerName) {
+        if (this.isConnected()) {
+            this.send({
+                type: 'join',
+                name: playerName
+            });
+        }
+    }
+
+    sendMove(position, rotation) {
+        if (this.isConnected()) {
+            this.send({
+                type: 'move',
+                x: position.x,
+                y: position.y,
+                z: position.z,
+                rotation_x: rotation.x,
+                rotation_y: rotation.y
+            });
+        }
+    }
+
+    sendShoot(target) {
+        if (this.isConnected()) {
+            this.send({
+                type: 'shoot',
+                target_x: target.x,
+                target_y: target.y,
+                target_z: target.z
+            });
+        }
+    }
+
+    sendHit(targetPlayerId, wasKilled) {
+        if (this.isConnected()) {
+            this.send({
+                type: 'hit',
+                target_player_id: targetPlayerId,
+                killed: wasKilled
+            });
+        }
+    }
+
+    send(data) {
+        if (this.ws && this.ws.readyState === WebSocket.OPEN) {
+            this.ws.send(JSON.stringify(data));
+        }
+    }
+
+    isConnected() {
+        return this.connected && this.ws && this.ws.readyState === WebSocket.OPEN;
+    }
+
+    onPlayerJoined(callback) {
+        this.onPlayerJoinedCallback = callback;
+    }
+
+    onPlayerLeft(callback) {
+        this.onPlayerLeftCallback = callback;
+    }
+
+    onPlayerMoved(callback) {
+        this.onPlayerMovedCallback = callback;
+    }
+
+    onPlayerShot(callback) {
+        this.onPlayerShotCallback = callback;
+    }
+    
+    onPlayerHit(callback) {
+        this.onPlayerHitCallback = callback;
+    }
+    
+    onPlayerDied(callback) {
+        this.onPlayerDiedCallback = callback;
+    }
+    
+    onPlayerRespawned(callback) {
+        this.onPlayerRespawnedCallback = callback;
+    }
+}
